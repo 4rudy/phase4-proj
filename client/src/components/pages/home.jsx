07c69164d1290e08720ed14c0936f226d36456b5
matthew <shaggysci3@gmail.com>
2023-12-20 14:13:03 -0500

    added home,create,quests to app made them have stuff on them like an img and form

---
+++ client/src/components/pages/home.jsx
@@ -0,0 +1,15 @@
+
+function Home(){
+    // code here for functionality
+
+   return(
+    // html react code
+    <>
+    <div>
+        <h1>WELCOME</h1>
+    </div>
+    </>
+   ) 
+}
+
+export default Home;
---
+++ client/src/components/pages/quest.jsx
@@ -0,0 +1,17 @@
+
+
+
+function Quest(){
+    // code here for functionality
+
+   return(
+    // html react code
+    <>
+     <div>
+        <img src="https://th.bing.com/th/id/OIG.ORcQsUJG7NhqG18OU1Um?w=1024&h=1024&rs=1&pid=ImgDetMain"></img>
+    </div>
+    </>
+   ) 
+}
+
+export default Quest;
