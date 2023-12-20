e823013024b5954fb640100f034fd3a1e1a9dfe7
Shal Malhotra <smalhotra0109@gmail.com>
2023-12-20 14:15:12 -0500

    routes added

--- server/pages/routes.jsx
+++ server/pages/routes.jsx
@@ -1,13 +1,23 @@
 import Home from "./pages/Home";
 import ErrorPage from "./pages/ErrorPage";
 import CharacterCreator from "./pages/CharacterCreator";
-
+import App from "./pages/App";
 
 const routes = [
     {
         path: "/",
         element: <App />,
         errorElement: <ErrorPage />,
+        children: [
+            {
+                path: "/",
+                element: < Home />,
+             },
+             {
+                path: "/characters",
+                element: < CharacterCreator />
+             }
+        ]
 
     }
 ]
